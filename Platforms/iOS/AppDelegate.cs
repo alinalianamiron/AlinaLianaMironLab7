cce0382edf819b5d1722daa9227521313b4ac841
Alina Miron <alina.liana.miron@gmail.com>
2024-12-07 12:19:34 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AlinaLianaMironLab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
